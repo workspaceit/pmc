9bd4e17bc77e8040a23fed4a5a17262daa45a35b
Anik S <aniksarker17@gmail.com>
2018-01-02 16:31:12 +0600

    login page design fix 2

--- src/main/java/com/workspaceit/pmc/config/SecurityConfig.java
+++ src/main/java/com/workspaceit/pmc/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+//@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
