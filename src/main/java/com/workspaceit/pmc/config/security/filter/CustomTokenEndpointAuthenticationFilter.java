36d518abd27923184cfbde7645315f2938c80ae5
wsitrafi <rafi@workspaceit.com>
2018-02-09 18:51:31 +0600

    After oatuh user type check

--- src/main/java/com/workspaceit/pmc/config/security/filter/CustomTokenEndpointAuthenticationFilter.java
+++ src/main/java/com/workspaceit/pmc/config/security/filter/CustomTokenEndpointAuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.workspaceit.pmc.config.security.filter;
 
+import com.workspaceit.pmc.auth.AdminUserDetails;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpointAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -24,6 +27,13 @@ public class CustomTokenEndpointAuthenticationFilter extends TokenEndpointAuthen
     protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException {
 
                 /* on successful authentication do stuff here */
+
+             Object user = authResult.getPrincipal();
+
+             if(user instanceof AdminUserDetails){
+                 new SecurityContextLogoutHandler().logout(request, null, null);
+                 throw new BadCredentialsException("Admin login not allowed here. Use admin panel");
+             }
 
     }
 
